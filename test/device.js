a0f1bf2e5e423f0dc37637a45b618b354ecbf76c
CodeColorist <codecolorist@gmail.com>
2019-07-02 01:00:09 +0800

    clean up proxy

--- test/device.js
+++ test/device.js
@@ -8,6 +8,7 @@ const { connect, proxy } = require('../lib/rpcv2')
 chai.use(chaiAsPromised)
 const { expect } = chai
 
+
 describe('RPC', () => {
   let device, session, api
   beforeEach(async () => {
@@ -22,20 +23,25 @@ describe('RPC', () => {
     api = proxy(__exports)
   })
 
-  it('invoke RPC', async () => {
+  it('should handle basic RPC usage', async () => {
     expect(await api('cookies/list')).to.be.an('array')
+
+    expect(await api.cookies.list()).to.be.an('array')
     expect(await api.checksec()).to.be.an('object')
       .and.to.has.keys(['entitlements', 'encrypted', 'arc', 'canary', 'pie'])
+
+    expect(api.non.exist()).to.be.rejected
+    expect(() => delete api.symbol).to.be.throw
+    expect(() => api.foo = 'bar').to.be.throw
+  })
+
+  it('should covcer the modules', async () => {
     expect(await api.info.info()).to.be.an('object')
       .and.to.has.keys(['tmp', 'home', 'json', 'id', 'bundle', 'binary', 'urls', 'minOS', 'name', 'semVer', 'version'])
     expect(await api.info.userDefaults()).to.be.an('object')
     expect(await api.symbol.modules()).to.be.an('array')
     expect(await api.symbol.imports('MobileSafari')).to.be.an('array')
     expect(await api.symbol.exports('WebKit')).to.be.an('array')
-  })
-  
-  it('should reject non extsting call', async () => {
-    expect(api('cookies/non-exist')).to.be.rejected
   })
 
   afterEach(async () => {
