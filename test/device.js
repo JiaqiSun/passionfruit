e89eb9f1d7eb6ba30d00f7886ea22c3897268435
CodeColorist <codecolorist@gmail.com>
2019-07-02 23:42:42 +0800

    clean up proxy implementation

--- test/device.js
+++ test/device.js
@@ -10,7 +10,7 @@ const { expect } = chai
 
 
 describe('RPC', () => {
-  let device, session, api
+  let device, session, rpc
   beforeEach(async () => {
     device = await frida.getUsbDevice()
     try {
@@ -20,28 +20,28 @@ describe('RPC', () => {
     }
     const __exports = await connect(session)
     // console.log(await __exports.interfaces())
-    api = proxy(__exports)
+    rpc = proxy(__exports)
   })
 
   it('should handle basic RPC usage', async () => {
-    expect(await api('cookies/list')).to.be.an('array')
+    expect(await rpc('cookies/list')).to.be.an('array')
 
-    expect(await api.cookies.list()).to.be.an('array')
-    expect(await api.checksec()).to.be.an('object')
+    expect(await rpc.cookies.list()).to.be.an('array')
+    expect(await rpc.checksec()).to.be.an('object')
       .and.to.has.keys(['entitlements', 'encrypted', 'arc', 'canary', 'pie'])
 
-    expect(api.non.exist()).to.be.rejected
-    expect(() => delete api.symbol).to.be.throw
-    expect(() => api.foo = 'bar').to.be.throw
+    expect(rpc.non.exist()).to.be.rejected
+    expect(() => delete rpc.symbol).to.be.throw
+    expect(() => rpc.foo = 'bar').to.be.throw
   })
 
   it('should covcer the modules', async () => {
-    expect(await api.info.info()).to.be.an('object')
+    expect(await rpc.info.info()).to.be.an('object')
       .and.to.has.keys(['tmp', 'home', 'json', 'id', 'bundle', 'binary', 'urls', 'minOS', 'name', 'semVer', 'version'])
-    expect(await api.info.userDefaults()).to.be.an('object')
-    expect(await api.symbol.modules()).to.be.an('array')
-    expect(await api.symbol.imports('MobileSafari')).to.be.an('array')
-    expect(await api.symbol.exports('WebKit')).to.be.an('array')
+    expect(await rpc.info.userDefaults()).to.be.an('object')
+    expect(await rpc.symbol.modules()).to.be.an('array')
+    expect(await rpc.symbol.imports('MobileSafari')).to.be.an('array')
+    expect(await rpc.symbol.exports('WebKit')).to.be.an('array')
   })
 
   afterEach(async () => {
