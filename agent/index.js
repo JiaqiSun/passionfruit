6db4cabc1669ccad25284ff4a35fa9121ecb22ce
CodeColorist <codecolorist@gmail.com>
2019-07-03 23:53:54 +0800

    fix unused global variable

--- agent/index.js
+++ agent/index.js
@@ -10,21 +10,17 @@ rpc.exports = {
 
 function registerModules() {
   const context = require.context('./modules', false, /\.js$/)
-  for (let filename of context.keys()) {
+  for (const filename of context.keys()) {
     const mod = context(filename)
-    const [_, name] = /\.\/(\S+)\.js/.exec(filename)
-    modules[name] = {}
-    for (let [method, func] of Object.entries(mod)) {
+    const [, name] = /\.\/(\S+)\.js/.exec(filename)
+    for (const [method, func] of Object.entries(mod)) {
       if (method === 'default') {
         register(func, name)
-        modules[name] = func
       } else {
         register(func, [name, func.name].join('/'))
-        modules[name][method] = func
       }
     }
   }
 }
 
-global.modules = {}
 registerModules()
