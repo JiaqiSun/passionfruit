ec6a29829222182a6b1321445e6f5eb51702afae
codecolorist <codecolorist@gmail.com>
2019-07-03 22:16:26 +0800

    check for minimal frida runtime

--- agent/ready.js
+++ agent/ready.js
@@ -1,1 +1,13 @@
+function requireMinimalVersion(requirement) {
+  const parse = ver => ver.split('.').map(s => parseInt(s, 10))
+  const a = parse(Frida.version), b = parse(requirement)
+  for (let i = 0; i < Math.max(a.length, b.length); i++)
+    if (a[i] < b[i])
+      throw new Error(`Fatal error: requiring mininal frida
+ version ${requirement}, found ${Frida.version}`)
+
+}
+
+requireMinimalVersion('12.5')
+
 Module.ensureInitialized('Foundation')
