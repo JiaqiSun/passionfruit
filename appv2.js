3abaad46821ec9830b6dc7fb902f650eac67d9da
CodeColorist <codecolorist@gmail.com>
2019-07-07 16:32:42 +0800

    new server implementation

---
+++ appv2.js
@@ -0,0 +1,84 @@
+import Koa from 'koa'
+import logger from 'koa-logger'
+import json from 'koa-json'
+import bodyParser from 'koa-bodyparser'
+import send from 'koa-send'
+import Router from 'koa-router'
+import path from 'path'
+
+import { createServer } from 'http'
+import { Device } from './lib/device.js'
+import { broadcast, attach, detach } from './lib/channelsv2'
+
+
+export const app = new Koa()
+const router = new Router({ prefix: '/api' })
+
+router
+  .get('/devices', async ctx => {
+    ctx.body = {
+      version: Device.frida.version,
+      list: await Device.list()
+    }
+  })
+  .get('/device/:device/apps', async (ctx) => {
+    const device = await Device.get(ctx.params.device)
+    ctx.body = device.apps()
+  })
+  .post('/url/start', async (ctx) => {
+    const { device, bundle, url } = ctx.request.body
+    const dev = await Device.get(device)
+    const pid = await dev.open(bundle, url)
+    ctx.body = { status: 'ok', pid }
+  })
+
+app.use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(json({
+    pretty: false,
+    param: 'pretty',
+  }))
+} else {
+  app.use(async (ctx, next) => {
+    const opt = { root: path.join(__dirname, 'gui') }
+    if (ctx.path.startsWith('/static/'))
+      await send(ctx, ctx.path, opt)
+    else
+      await send(ctx, '/index.html', opt)
+    next()
+  })
+  app.use(logger())
+}
+
+export const server = createServer(app.callback())
+
+export function start(opt) {
+  attach(server)
+  process.on('unhandledRejection', err =>
+    broadcast('unhandledRejection', {
+      err: err.toString(),
+      stack: err.stack,
+    }))
+
+  return new Promise(resolve =>
+    server.listen(opt, () => {
+      const addr = server.address()
+      const host = addr.address.replace(/^::$/, 'localhost')
+      console.info(`listening on http://${host}:${addr.port}`.green)
+      resolve(server)
+    }))
+}
+
+export function stop() {
+  detach(server)
+  server.close()
+}
+
+process.on('unhandledRejection', err => {
+  console.error('An unhandledRejection occurred: '.red)
+  console.error(`Rejection: ${err}`.red)
+  console.error(err.stack)
+})
---
+++ test/web.js
@@ -0,0 +1,36 @@
+import request from 'supertest'
+
+import { expect } from 'chai'
+import { server, start, stop } from '../appv2'
+
+import { Device } from '../lib/device'
+
+describe('Web', () => {
+  before(async () => {
+    await start()
+  })
+
+  it('should server the page and rest api', async() => {
+    request(server).get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+
+    const usb = await Device.usb()
+    const device = usb.device.id
+    const { body } = await request(server).get('/api/devices')
+      .expect('Content-Type', /json/)
+    expect(body).has.key('version', 'list')
+    expect(body.list.find(dev => dev.id === device)).to.be.an('object')
+    
+    await request(server).get(`/device/${usb.device.id}/apps`).expect(200)
+    await request(server).post('/api/url/start').send({
+      device,
+      bundle: 'com.apple.mobilesafari',
+      url: 'about:blank'
+    }).expect(200)
+  })
+
+  after(async () => {
+    await stop()
+  })
+})
